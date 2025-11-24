9ad4ab359503c39312e4bdf94a7c27e4613f4447
20095949-mateus-campos <20095949@mydbs.ie>
2025-11-24 21:42:12 +0000

    CRUD elements for Product

--- src/App.jsx
+++ src/App.jsx
@@ -38,27 +38,31 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>The current time is {new Date(currentTime * 1000).toLocaleString()}.</p>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>CREATE:</p>
+      <form action={create}>
+        <input placeholder="prodcut name" name="productName"></input>
+        <button type="submit" name="button" value="submit">Create</button>
+      </form>
+
+      <p>READ:</p>
+      <form action={read}>
+        <input placeholder="prodcut id" name="productId"></input>
+        <button type="submit" name="button" value="submit">Read</button>
+      </form>
+      {Object.keys(product) != 0 ? <p>ID: {product.id} - Name: {product.name}</p> : <p>Not Found</p>}
+
+      <p>UPDATE:</p>
+      <form action={update}>
+        <input placeholder="prodcut id" name="productId"></input>
+        <input placeholder="new prodcut name" name="productName"></input>
+        <button type="submit" name="button" value="submit">Update</button>
+      </form>
+
+      <p>DELETE:</p>
+      <form action={deleteProduct}>
+        <input placeholder="prodcut id" name="productId"></input>
+        <button type="submit" name="button" value="submit">Delete</button>
+      </form>
     </>
   )
 }
