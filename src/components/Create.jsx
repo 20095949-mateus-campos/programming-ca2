38f2cd9608e77f091f11e01f6b26f66a05bd2f58
20095949-mateus-campos <20095949@mydbs.ie>
2025-11-25 20:50:52 +0000

    added Create component (based on https://github.com/mateusfonseca/dorsetToDo, an old project of mine)

---
+++ src/components/Create.jsx
@@ -0,0 +1,19 @@
+export default function Create() {
+    return (
+        <>
+            <div class="hero-head">
+                <nav class="navbar">
+                    <div class="container">
+                        <div id="navbarMenuHeroA" class="navbar-menu">
+                            <div class="navbar-end">
+                                <a class="navbar-item">Products</a>
+                                <a class="navbar-item">Work Orders</a>
+                                <a class="navbar-item">Clients</a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </>
+    )
+}
