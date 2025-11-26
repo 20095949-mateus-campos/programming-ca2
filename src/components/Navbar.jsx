2d255037345ea2749b3e80f50dc4cdc18478008b
20095949-mateus-campos <20095949@mydbs.ie>
2025-11-26 00:02:00 +0000

    chaned class to className, React was complaining

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 export default function Navbar({funcs}) {
     return (
         <>
-            <div class="hero-head">
-                <nav class="navbar">
-                    <div class="container">
-                        <div id="navbarMenuHeroA" class="navbar-menu">
-                            <div class="navbar-end">
-                                <a class="navbar-item" type="button" onClick={funcs[0]}>Home</a>
-                                <a class="navbar-item" onClick={funcs[1]}>Products</a>
-                                <a class="navbar-item" onClick={funcs[1]}>Work Orders</a>
-                                <a class="navbar-item" onClick={funcs[1]}>Clients</a>
+            <div className="hero-head">
+                <nav className="navbar">
+                    <div className="container">
+                        <div id="navbarMenuHeroA" className="navbar-menu">
+                            <div className="navbar-end">
+                                <a className="navbar-item" onClick={home}>Home</a>
+                                <a className="navbar-item" onClick={() => read("product")}>Products</a>
+                                <a className="navbar-item" onClick={() => read("work-order")}>Work Orders</a>
+                                <a className="navbar-item" onClick={() => read("client")}>Clients</a>
                             </div>
                         </div>
                     </div>
