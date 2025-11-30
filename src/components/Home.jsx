75c312a1919513c80de576fd690b77ed3c0f45e2
20095949-mateus-campos <20095949@mydbs.ie>
2025-11-30 23:41:30 +0000

    changed class to className

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,25 @@
 export default function Home({read}) {
     return (
         <>
-            <nav class="panel">
-                <p class="panel-heading">ERP</p>
-                    <div class="panel-block">
-                    <button class="button is-link is-outlined is-fullwidth" onClick={() => read("product")}>Products</button>
+            <nav className="panel">
+                <p className="panel-heading">ERP</p>
+                    <div className="panel-block">
+                    <button className="button is-link is-outlined is-fullwidth" onClick={() => read("product")}>Products</button>
                     </div>
-            <div class="panel-block">
-            <button class="button is-link is-outlined is-fullwidth" onClick={() => read("workorder")}>Work Orders</button>
+            <div className="panel-block">
+            <button className="button is-link is-outlined is-fullwidth" onClick={() => read("workorder")}>Work Orders</button>
             </div>
-            <div class="panel-block">
-            <button class="button is-link is-outlined is-fullwidth" onClick={() => read("client")}>Clients</button>
+            <div className="panel-block">
+            <button className="button is-link is-outlined is-fullwidth" onClick={() => read("client")}>Clients</button>
             </div>
-            <div class="panel-block">
-            <button class="button is-link is-outlined is-fullwidth" onClick={() => read("process")}>Processes</button>
+            <div className="panel-block">
+            <button className="button is-link is-outlined is-fullwidth" onClick={() => read("process")}>Processes</button>
             </div>
-            <div class="panel-block">
-            <button class="button is-link is-outlined is-fullwidth" onClick={() => read("material")}>Materials</button>
+            <div className="panel-block">
+            <button className="button is-link is-outlined is-fullwidth" onClick={() => read("material")}>Materials</button>
             </div>
-            <div class="panel-block">
-            <button class="button is-link is-outlined is-fullwidth" onClick={() => read("tool")}>Tools</button>
+            <div className="panel-block">
+            <button className="button is-link is-outlined is-fullwidth" onClick={() => read("tool")}>Tools</button>
             </div>
             </nav>
         </>
