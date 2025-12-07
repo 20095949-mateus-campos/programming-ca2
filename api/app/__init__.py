5577843319c84b6e2ca4cb59ad6df0fecd9b80ec
20095949-mateus-campos <20095949@mydbs.ie>
2025-12-07 01:52:50 +0000

    using app factory for testing purposes

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,13 +3,25 @@
 # Changes have been commented where applicable.
 
 from flask import Flask
-from .config import Config
+from .config import *
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy()
+
+def create_app(testing=False):
+    app = Flask(__name__)
+    
+    if testing:
+        app.config.from_object(TestingConfig)        
+    else:
+        app.config.from_object(DevelopmentConfig)
+    
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    return [app, db]
+
+app, db = create_app()
 
 from app import routes, models
