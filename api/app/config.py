61c0a4ef1f6a29cd024cba680a5fb4be2b5ac378
20095949-mateus-campos <20095949@mydbs.ie>
2025-12-07 01:53:17 +0000

    added secondary config for testing purposes

--- api/app/config.py
+++ api/app/config.py
@@ -5,7 +5,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
-    # ...
+class DevelopmentConfig():
+    TESTING = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+
+class TestingConfig():
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
