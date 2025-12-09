2bb30aae13276a5c5d021e980c915f28746b9a0e
20095949-mateus-campos <20095949@mydbs.ie>
2025-12-09 00:17:23 +0000

    removed unreachable code

--- api/app/routes.py
+++ api/app/routes.py
@@ -12,14 +12,10 @@ from sqlalchemy.sql.sqltypes import DateTime
 blueprint = Blueprint('blueprint', __name__)
 
 # this function capitalizes the entity name following:
-#   'bom' -> 'BOM'
-#   'bop' -> 'BOP'
 #   'workorder' -> 'WorkOrder'
 #   anything else, capitalize just the first letter, ex.: 'product' -> 'Product'
 def solve_entity(entity):
-    if entity in ['bom', 'bop']:
-        return str.upper(entity)
-    elif entity == 'workorder':
+    if entity == 'workorder':
         return ''.join(list(map(lambda x: str.capitalize(x), [entity[:4], entity[4:]])))
     else:
         return str.capitalize(entity)
