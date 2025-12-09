1dbef3a84926cd6614491347a4bfe16653028740
20095949-mateus-campos <20095949@mydbs.ie>
2025-12-09 00:14:50 +0000

    added test cases for workorder, process and product

--- api/app/tests/crud_test.py
+++ api/app/tests/crud_test.py
@@ -118,3 +118,199 @@ def test_crud_client(clnt, ctxt):
         # Delete one
         response = json.loads(clnt.delete('/api/delete/client/1').text)
         assert len(db.session.query(Client).all()) == 0
+
+# test function for CRUD operations for the WorkOrder entity
+def test_crud_workorder(clnt, ctxt):
+    old_model = {'client': 1, 'product': 1, 'start': '2025-10-10', 'end': '2025-11-10', 'cost': 500.00}
+    new_model = {'client': 2, 'product': 2, 'start': '2026-11-25', 'end': '2026-12-25', 'cost': 1000.00}
+
+    # Satisfy foreign keys
+    db.session.add(Client(name='any', email='any', phone='any', address='any'))
+    db.session.add(Client(name='any', email='any', phone='any', address='any'))
+    db.session.add(Product(name='any', blueprint='any'))
+    db.session.add(Product(name='any', blueprint='any'))
+    db.session.commit()
+
+    with ctxt:
+        # Read all when none
+        response = json.loads(clnt.get('/api/read/workorder/0').text)
+        assert len(db.session.query(WorkOrder).all()) == 0
+        assert response == []
+
+        # Read one when none
+        response = json.loads(clnt.get('/api/read/workorder/1').text)
+        assert response == {}
+        
+        # Create one
+        response = json.loads(clnt.post('/api/create/workorder', json=old_model).text)
+        assert len(db.session.query(WorkOrder).all()) == 1
+        model = db.session.get_one(WorkOrder, 1)
+        assert model.client == old_model['client']
+        assert model.product == old_model['product']
+        assert model.start == datetime.strptime(old_model['start'], '%Y-%m-%d')
+        assert model.end == datetime.strptime(old_model['end'], '%Y-%m-%d')
+        assert model.cost == old_model['cost']
+        assert response['client'] == old_model['client']
+        assert response['product'] == old_model['product']
+        assert response['start'] == old_model['start']
+        assert response['end'] == old_model['end']
+        assert response['cost'] == old_model['cost']
+
+        # Read all
+        response = json.loads(clnt.get('/api/read/workorder/0').text)
+        assert len(db.session.query(WorkOrder).all()) == 1
+        assert response[0]['client'] == old_model['client']
+        assert response[0]['product'] == old_model['product']
+        assert response[0]['start'] == old_model['start']
+        assert response[0]['end'] == old_model['end']
+        assert response[0]['cost'] == old_model['cost']
+
+        # Read one
+        response = json.loads(clnt.get('/api/read/workorder/1').text)
+        assert response['client'] == old_model['client']
+        assert response['product'] == old_model['product']
+        assert response['start'] == old_model['start']
+        assert response['end'] == old_model['end']
+        assert response['cost'] == old_model['cost']
+
+        # Update one
+        response = json.loads(clnt.patch('/api/update/workorder/1', json=new_model).text)
+        model = db.session.get_one(WorkOrder, 1)
+        assert model.client == new_model['client']
+        assert model.product == new_model['product']
+        assert model.start == datetime.strptime(new_model['start'], '%Y-%m-%d')
+        assert model.end == datetime.strptime(new_model['end'], '%Y-%m-%d')
+        assert model.cost == new_model['cost']
+        assert response['client'] == new_model['client']
+        assert response['product'] == new_model['product']
+        assert response['start'] == new_model['start']
+        assert response['end'] == new_model['end']
+        assert response['cost'] == new_model['cost']
+
+        # Delete one
+        response = json.loads(clnt.delete('/api/delete/workorder/1').text)
+        assert len(db.session.query(WorkOrder).all()) == 0
+
+# test function for CRUD operations for the Process entity
+def test_crud_process(clnt, ctxt):
+    old_model = {'name': 'Test Process', 'description': 'Test process description', 'tool': {'Use': 1}}
+    new_model = {'name': 'New Test Process', 'description': 'New test process description', 'tool': {'Use': 2}}
+
+    # Satisfy foreign keys
+    db.session.add(Tool(name='any'))
+    db.session.add(Tool(name='any'))
+    db.session.commit()
+
+    with ctxt:
+        # Read all when none
+        response = json.loads(clnt.get('/api/read/process/0').text)
+        assert len(db.session.query(Process).all()) == 0
+        assert response == []
+
+        # Read one when none
+        response = json.loads(clnt.get('/api/read/process/1').text)
+        assert response == {}
+        
+        # Create one
+        response = json.loads(clnt.post('/api/create/process', json=old_model).text)
+        assert len(db.session.query(Process).all()) == 1
+        model = db.session.get_one(Process, 1)
+        assert model.name == old_model['name']
+        assert model.description == old_model['description']
+        assert db.session.execute(db.select(Use).filter_by(process=model.id)).scalar_one().tool == old_model['tool']['Use']
+        assert response['name'] == old_model['name']
+        assert response['description'] == old_model['description']
+        assert response['tool'] == old_model['tool']['Use']
+
+        # Read all
+        response = json.loads(clnt.get('/api/read/process/0').text)
+        assert len(db.session.query(Process).all()) == 1
+        assert response[0]['name'] == old_model['name']
+        assert response[0]['description'] == old_model['description']
+
+        # Read one
+        response = json.loads(clnt.get('/api/read/process/1').text)
+        assert response['name'] == old_model['name']
+        assert response['description'] == old_model['description']
+        assert response['tool'] == old_model['tool']['Use']
+
+        # Update one
+        response = json.loads(clnt.patch('/api/update/process/1', json=new_model).text)
+        model = db.session.get_one(Process, 1)
+        assert model.name == new_model['name']
+        assert model.description == new_model['description']
+        assert db.session.execute(db.select(Use).filter_by(process=model.id)).scalar_one().tool == new_model['tool']['Use']
+        assert response['name'] == new_model['name']
+        assert response['description'] == new_model['description']
+        assert response['tool'] == new_model['tool']['Use']
+
+        # Delete one
+        response = json.loads(clnt.delete('/api/delete/process/1').text)
+        assert len(db.session.query(Process).all()) == 0
+
+# test function for CRUD operations for the Product entity
+def test_crud_product(clnt, ctxt):
+    old_model = {'name': 'Test Product', 'blueprint': 'image-uri', 'process': {'BOP': 1}, 'material': {'BOM': 1}}
+    new_model = {'name': 'New Test Product', 'blueprint': 'new-image-uri', 'process': {'BOP': 2}, 'material': {'BOM': 2}}
+
+    # Satisfy foreign keys
+    db.session.add(Process(name='any', description='any'))
+    db.session.add(Process(name='any', description='any'))
+    db.session.add(Tool(name='any'))
+    db.session.add(Use(process=1, tool=1))
+    db.session.add(Use(process=2, tool=1))
+    db.session.add(Material(name='any'))
+    db.session.add(Material(name='any'))
+    db.session.commit()
+
+    with ctxt:
+        # Read all when none
+        response = json.loads(clnt.get('/api/read/product/0').text)
+        assert len(db.session.query(Product).all()) == 0
+        assert response == []
+
+        # Read one when none
+        response = json.loads(clnt.get('/api/read/product/1').text)
+        assert response == {}
+        
+        # Create one
+        response = json.loads(clnt.post('/api/create/product', json=old_model).text)
+        assert len(db.session.query(Product).all()) == 1
+        model = db.session.get_one(Product, 1)
+        assert model.name == old_model['name']
+        assert model.blueprint == old_model['blueprint']
+        assert db.session.execute(db.select(BOP).filter_by(product=model.id)).scalar_one().process == old_model['process']['BOP']
+        assert db.session.execute(db.select(BOM).filter_by(product=model.id)).scalar_one().material == old_model['material']['BOM']
+        assert response['name'] == old_model['name']
+        assert response['blueprint'] == old_model['blueprint']
+        assert response['process'] == old_model['process']['BOP']
+        assert response['material'] == old_model['material']['BOM']
+
+        # Read all
+        response = json.loads(clnt.get('/api/read/product/0').text)
+        assert len(db.session.query(Product).all()) == 1
+        assert response[0]['name'] == old_model['name']
+        assert response[0]['blueprint'] == old_model['blueprint']
+
+        # Read one
+        response = json.loads(clnt.get('/api/read/product/1').text)
+        assert response['name'] == old_model['name']
+        assert response['blueprint'] == old_model['blueprint']
+        assert response['process'] == old_model['process']['BOP']
+        assert response['material'] == old_model['material']['BOM']
+
+        # Update one
+        response = json.loads(clnt.patch('/api/update/product/1', json=new_model).text)
+        model = db.session.get_one(Product, 1)
+        assert model.name == new_model['name']
+        assert model.blueprint == new_model['blueprint']
+        assert db.session.execute(db.select(BOP).filter_by(product=model.id)).scalar_one().process == new_model['process']['BOP']
+        assert db.session.execute(db.select(BOM).filter_by(product=model.id)).scalar_one().material == new_model['material']['BOM']
+        assert response['name'] == new_model['name']
+        assert response['blueprint'] == new_model['blueprint']
+        assert response['process'] == new_model['process']['BOP']
+        assert response['material'] == new_model['material']['BOM']
+
+        # Delete one
+        response = json.loads(clnt.delete('/api/delete/product/1').text)
+        assert len(db.session.query(Product).all()) == 0
