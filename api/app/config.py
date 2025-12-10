86f9c3f746426af4832e5b73f55be89a8b90d700
20095949-mateus-campos <20095949@mydbs.ie>
2025-12-10 22:32:32 +0000

    added comments and attributions

--- api/app/config.py
+++ api/app/config.py
@@ -2,6 +2,8 @@
 # Link: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database.
 # Changes have been commented where applicable.
 
+# Flask Docs referenced: https://flask.palletsprojects.com/en/stable/config/#development-production.
+
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,6 +12,7 @@ class DevelopmentConfig():
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
 
+# added testing configuration which uses in-memory database
 class TestingConfig():
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
