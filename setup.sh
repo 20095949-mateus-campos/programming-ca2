9154849e02449b424cf9f1be2108bda415a4f99e
20095949-mateus-campos <20095949@mydbs.ie>
2025-12-10 22:10:13 +0000

    exporting nvm before using it without reopening the terminal

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # Install Node.js 24.11+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install v24.11.1
 
 # Start and build frontend
